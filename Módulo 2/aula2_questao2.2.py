290eb2549877b04d8418f2fa3f00987e12621cfc
Flavio059 <163608029+Flavio059@users.noreply.github.com>
2024-05-14 21:55:16 -0300

    Add files via upload

--- Módulo 2/aula2_questao2.2.py
+++ Módulo 2/aula2_questao2.2.py
@@ -1,1 +1,47 @@
 
+v1 = 5
+v2 = 2
+print(type(v1))
+print(type(v2))
+
+divisão = v1/v2
+print(divisão)
+print(type(divisão))
+
+r = ("o resultado é")
+print(r)
+v1 = 10
+print(v1)
+v2 = 3.5
+print(v2)
+r1 = v1 + v2
+print(r1)
+
+print(type(r))
+print(type(v1))
+print(type(v2))
+print(type(r1))
+
+v1 = 10
+v2 = 20
+aux = v1
+v1 = v2
+v2 = aux
+print("Novo valor de v1:", v1)
+print("Novo valor de v2:", v2)
+
+
+juros = 1.01
+saldo = 500.0
+print(saldo)
+saldo = saldo * juros #505 rendimento1
+print(saldo)
+saldo = saldo * juros # rendimento2
+print(saldo)
+saldo = saldo * juros # rendimento3
+print(saldo)
+
+print(type(saldo))
+
+
+
